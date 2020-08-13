d7fe5d4b3b709bbfac6d1671064e947a076ffeb7
letanphuc1214 <tanphuc1214@gmail.com>
2020-08-13 15:36:03 +0700

    update

--- week2/BT_XayDungUngDungATM/src/Atm.java
+++ week2/BT_XayDungUngDungATM/src/Atm.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Atm {
     double taiKhoan = 50000;
     String name ;
-
+//    String[] histories =new String[3];
     public String getName() {
         return name;
     }
@@ -24,5 +24,10 @@ public class Atm {
     public void setTaiKhoan(double taiKhoan) {
         this.taiKhoan = taiKhoan;
     }
+//    public void getLichSu(String[] histories){
+//        for (int i = 0; i < histories.length; i++){
+//            System.out.println();
+//        }
+//    }
 
 }

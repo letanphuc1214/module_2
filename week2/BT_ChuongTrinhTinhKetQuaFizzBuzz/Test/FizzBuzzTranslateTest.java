abacb1af412c11bc4258005cd6bad575a7caa0e8
letanphuc1214 <tanphuc1214@gmail.com>
2020-08-18 15:40:47 +0700

    update

--- week2/BT_ChuongTrinhTinhKetQuaFizzBuzz/Test/FizzBuzzTranslateTest.java
+++ week2/BT_ChuongTrinhTinhKetQuaFizzBuzz/Test/FizzBuzzTranslateTest.java
@@ -4,25 +4,58 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzTranslateTest {
 
+//    @Test
+//    void translate() {
+//        int number = 10;
+//        String expected = "buzz";
+//        String result = FizzBuzzTranslate.translate(number);
+//        assertEquals(expected, result);
+//    }
+//    @Test
+//    void translate1() {
+//        int number = 7;
+//        String expected = "number: " + number;
+//        String result = FizzBuzzTranslate.translate(number);
+//        assertEquals(expected, result);
+//    }
+//    @Test
+//    void translate2() {
+//        int number = 9;
+//        String expected = "fizz";
+//        String result = FizzBuzzTranslate.translate(number);
+//        assertEquals(expected, result);
+//    }
+
     @Test
-    void translate() {
-        int number = 10;
-        String expected = "buzz";
-        String result = FizzBuzzTranslate.translate(number);
+    void containTranslate() {
+        String number = "93";
+        String expected = "chinfizz";
+        String result = "";
+        for (int i = 0; i < number.length(); i++){
+            result += FizzBuzzTranslate.containTranslate(String.valueOf(number.charAt(i)));
+        }
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void containTranslate1() {
+        String number = "59";
+        String expected = "buzzchin";
+        String result = "";
+        for (int i = 0; i < number.length(); i++){
+            result += FizzBuzzTranslate.containTranslate(String.valueOf(number.charAt(i)));
+        }
+
         assertEquals(expected, result);
     }
     @Test
-    void translate1() {
-        int number = 7;
-        String expected = "number: " + number;
-        String result = FizzBuzzTranslate.translate(number);
-        assertEquals(expected, result);
-    }
-    @Test
-    void translate2() {
-        int number = 9;
-        String expected = "fizz";
-        String result = FizzBuzzTranslate.translate(number);
+    void containTranslate2() {
+        String number = "20";
+        String expected = "haikhong";
+        String result = "";
+        for (int i = 0; i < number.length(); i++){
+            result += FizzBuzzTranslate.containTranslate(String.valueOf(number.charAt(i)));
+        }
         assertEquals(expected, result);
     }
 }
--- week2/BT_ChuongTrinhTinhKetQuaFizzBuzz/src/FizzBuzzTranslate.java
+++ week2/BT_ChuongTrinhTinhKetQuaFizzBuzz/src/FizzBuzzTranslate.java
@@ -1,8 +1,26 @@
 public class FizzBuzzTranslate {
-    public static String translate(int number){
-        if (number > 0 && number % 3 == 0) return "fizz";
-        if (number > 0 && number % 5 == 0) return "buzz";
-        if (number > 0 && number % 3 == 0 && number % 5 == 0) return "fizzbuzz";
-        return "number: " + number;
+//    public static String translate(int number) {
+//        if (number > 0 && number % 3 == 0) return "fizz";
+//        if (number > 0 && number % 5 == 0) return "buzz";
+//        if (number > 0 && number % 3 == 0 && number % 5 == 0) return "fizzbuzz";
+//        return "number: " + number;
+//    }
+
+    public static String containTranslate(String number) {
+        String result = switch (number) {
+            case "0" -> "khong";
+            case "1" -> "mot";
+            case "2" -> "hai";
+            case "3" -> "fizz";
+            case "4" -> "bon";
+            case "5" -> "buzz";
+            case "6" -> "sau";
+            case "7" -> "bay";
+            case "8" -> "tam";
+            case "9" -> "chin";
+            default -> "";
+        };
+        return result;
     }
+
 }

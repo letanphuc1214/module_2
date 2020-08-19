765bde377a6c7a5e26ab642385cb1a723fbff481
letanphuc1214 <tanphuc1214@gmail.com>
2020-08-19 08:16:41 +0700

    update

---
+++ week2/TH_TrienKhaiLopLishDonGian/src/MyListTest.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MyListTest {
+    public static void main(String[] args) {
+        MyList<Integer> listInteger = new MyList<Integer>();
+        listInteger.add(1);
+        listInteger.add(2);
+        listInteger.add(3);
+        listInteger.add(4);
+        listInteger.add(5);
+
+        for (int i = 0; i < listInteger.size; i++){
+            System.out.print(listInteger.get(i) + " ");
+        }
+        System.out.println("");
+        System.out.println("element 4: " + listInteger.get(4));
+        System.out.println("element 1: " + listInteger.get(1));
+        System.out.println("element 2: " + listInteger.get(2));
+
+
+        listInteger.add(6);
+        System.out.println("elment 6:" + listInteger.get(5));
+
+    }
+}

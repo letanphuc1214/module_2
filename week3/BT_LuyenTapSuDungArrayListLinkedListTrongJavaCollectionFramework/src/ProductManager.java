1ef2d4b5a156b8149f43a7a970d8ad309bec517c
letanphuc1214 <tanphuc1214@gmail.com>
2020-08-23 19:20:20 +0700

    update

--- week3/BT_LuyenTapSuDungArrayListLinkedListTrongJavaCollectionFramework/src/ProductManager.java
+++ week3/BT_LuyenTapSuDungArrayListLinkedListTrongJavaCollectionFramework/src/ProductManager.java
@@ -1,16 +1,15 @@
 import java.util.*;
 
 public class ProductManager {
-    public static ArrayList<Product> productList = new ArrayList<>();
-    public static Scanner scanner = new Scanner(System.in);
+    public static ArrayList<Product> productsList = new ArrayList<>();
     public static int idProduct = 0;
+    public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         int yourChoice = 10;
-
         while (yourChoice != 0) {
             menu();
-            System.out.print("Su lua chon cua ban la gi: ");
+            System.out.print("nhap lua chon cua ban: ");
             yourChoice = Integer.parseInt(scanner.next());
             switch (yourChoice) {
                 case 1:
@@ -23,7 +22,7 @@ public class ProductManager {
                     delete();
                     break;
                 case 4:
-                    showAllProduct();
+                    showProduct();
                     break;
                 case 5:
                     searchProductByName();
@@ -38,103 +37,101 @@ public class ProductManager {
                     exit();
                     break;
                 default:
-                    System.out.print("Nhap lai di ban oi");
-
+                    System.out.println("nhap lai di ban oi");
             }
-
         }
     }
 
     public static void menu() {
-        System.out.println("1. Thêm sản phẩm");
-        System.out.println("2. Sửa thông tin sản phẩm theo id");
-        System.out.println("3. Xoá sản phẩm theo id");
-        System.out.println("4. Hiển thị danh sách sản phẩm");
-        System.out.println("5. Tìm kiếm sản phẩm theo tên");
-        System.out.println("6. Sắp xếp sản phẩm  giảm dần theo giá");
-        System.out.println("7. Sắp xếp sản phẩm tăng dần theo giá");
+        System.out.println("1. Them san pham");
+        System.out.println("2. Sua thong tin san pham theo id");
+        System.out.println("3. Xoa san pham theo id");
+        System.out.println("4. Hien thi danh sach san pham");
+        System.out.println("5. Tim kiem san pham theo ten");
+        System.out.println("6. Sap xep san pham tang dan theo gia");
+        System.out.println("7. Sap xep san pham giam dan theo gia");
         System.out.println("0. Thoat");
-
     }
 
     public static void add() {
         Product product = new Product();
-        System.out.println("Nhap ten SP: ");
+        System.out.print("nhap ten san pham: ");
         product.name = scanner.next();
-        System.out.println("Nhap gia san pham: ");
+        System.out.print("nhap gia san pham: ");
         product.gia = Integer.parseInt(scanner.next());
         product.id = ++idProduct;
-        productList.add(product);
+        productsList.add(product);
     }
 
-    public static void exit() {
-        System.out.println("Chao tam biet...");
-        System.exit(0);
+    public static void delete() {
+        System.out.print("nhap id can xoa: ");
+        Product product = checkId(scanner.nextInt());
+        productsList.remove(product);
 
     }
 
-    public static void showAllProduct() {
-        for (Product product : productList) {
+    public static void showProduct() {
+        for (Product product : productsList) {
             System.out.println(product.toString());
         }
     }
 
-    public static void edit(){
-        System.out.print("Nhap id can sua: ");
-        Product product = checkId(scanner.nextInt());
-        System.out.print("nhap ten san pham can sua: ");
-        product.setName(scanner.next());
-        System.out.print("nhap gia san pham can sua: ");
-        product.setGia(scanner.nextInt());
-
-    }
-
-    public static void delete(){
-        System.out.println("nhap id can xoa: ");
-        Product product = checkId(scanner.nextInt());
-        productList.remove(product);
-    }
-
-    public static Product checkId(int idProduct){
-        for (Product product : productList){
-            if (idProduct == product.getId()){
+    public static Product checkId(int idProduct) {
+        for (Product product : productsList) {
+            if (idProduct == product.getId()) {
                 return product;
             }
         }
         return null;
     }
 
+    public static void edit() {
+        System.out.print("nhap id can sua: ");
+        Product product = checkId(scanner.nextInt());
+        System.out.print("nhap ten san pham can sua: ");
+        product.setName(scanner.next());
+        System.out.println("nhap gia san pham can sua: ");
+        product.setGia(Integer.parseInt(scanner.next()));
+
+    }
+
+    public static void exit() {
+        System.out.println("Chao tam biet");
+        System.exit(0);
+    }
+
+    public static void sortProductDecrease() {
+        Collections.sort(productsList, new SortProductDecrease());
+        System.out.println("San pham giam dan theo gia: ");
+        showProduct();
+    }
+
+    public static void sortProductIncrease(){
+        Collections.sort(productsList, new SortProductIncrease());
+        System.out.println("San pham tang dan theo gia: ");
+        showProduct();
+    }
+
     public static void searchProductByName(){
-        System.out.print("nhap ten san pham: ");
+        System.out.println("nhap ten san pham can tim: ");
         String name = scanner.next();
         if (checkName(name) >= 0){
-            System.out.println(productList.get(checkName(name)).toString());
+            System.out.println(productsList.get(checkName(name)).toString());
         }
         else {
-            System.out.println("khong co ten san pham nao nhu the ca");
+            System.out.println("khong co san pham nao ten do ");
         }
+
+
     }
 
     public static int checkName(String name){
-        for (int i = 0; i < productList.size(); i++){
-            if (name.equals(productList.get(i).getName())){
+        for (int i = 0; i < productsList.size(); i++){
+            if (name.equals(productsList.get(i).getName())){
                 return i;
             }
         }
         return -1;
+
     }
-
-    public static void sortProductDecrease(){
-        Collections.sort(productList, new SortProductDecrease());
-        System.out.println("Sap xep san pham theo gia giam dan: ");
-        showAllProduct();
-    }
-
-    public static void sortProductIncrease(){
-        Collections.sort(productList, new SortProductIncrease());
-        System.out.println("Sap xep san pham theo gia tang dan: ");
-        showAllProduct();
-    }
-
-
 }
